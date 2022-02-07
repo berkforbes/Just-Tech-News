81d4aff9cc00d105f3b976b2e39d9f92746eeeff
berkforbes <berkforbes@yahoo.com>
2022-02-06 20:30:03 -0700

    password verify

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    // Creates new user
     User.create({
       username: req.body.username,
       email: req.body.email,
@@ -50,6 +50,33 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.post('/login', (req, res) => {
+    // upon login, query user table to find one matching email, then assign req.body.email
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        // res.json({ user: dbUserData });
+    
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+    
+      });  
+    });
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
